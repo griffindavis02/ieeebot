fa4edf6c40c6772cc9bfab1549b26accaf272088
griffindavis02 <davisgriffinp@gmail.com>
2021-07-31 23:37:04 -0400

    Add email system for when bots finish

--- index.js
+++ index.js
@@ -1,46 +1,38 @@
 require('dotenv').config()
 const puppeteer = require('puppeteer')
-const delay = require('delay')
 const InstaBot = require('./InstaBot')
-const fetch = require('node-fetch')
+// const fetch = require('node-fetch')
+const spawn = require('child_process').spawn
 
 const options = {
     username: process.env.IG_USERNAME,
     password: process.env.IG_PASSWORD,
 
     dailyFollowLimit: 150,
-    hourlyFollowLimit: 20,
 
-    usersToFollowFollowersOf: ['novaengineer'],
+    usersToFollowFollowersOf: ['villanovaengineering'],
 
-    skipPrivate: true,
+    skipPrivate: false,
     dryRun: true
-};
+}
 
 async function startBot() {
     try {
-        let browser = await puppeteer.launch({ headless: false });
+        let browser = await puppeteer.launch({ headless: false })
 
-        const bot = await InstaBot.InstBot(options, browser);
+        const bot = await InstaBot.InstBot(options, browser)
 
-        await bot.FollowBot();
+        await bot.FollowBot()
+        const pythonProcess = spawn('python', ['./sendEmail.py'])
+        await browser.close()
     } catch (e) {
-        console.log(e);
+        console.log(e)
+        await browser.close()
     }
-    // finally {
-    //     console.log('Closing browser');
-    //     if (browser) await browser.close();
-    // }
-
-    // const userJSON = JSON.parse(await fetch('https://www.instagram.com/novaengineer/?__a=1')
-    //     .then(res => res.text())
-    //     .catch(err => console.log(err)));
-
-    // console.log(userJSON.graphql.user.edge_followed_by.count);
-    // use this method to see if follower is mutual or read button
-    // userJSON.graphql.user.followed_by_viewer
-    // userJSON.graphql.user.requested_by_viewer
-    // userJSON.graphql.user.follows_viewer
+    finally {
+        console.log('Closing browser')
+        if (browser) await browser.close()
+    }
 }
 
-startBot();
+startBot()
---
+++ sendEmail.py
@@ -0,0 +1,30 @@
+import smtplib
+import os
+import sys
+import json
+from dotenv import load_dotenv
+from email.message import EmailMessage
+# from email.MIMEMultipart import MIMEMultipart
+# from email.MIMEBase import MIMEBase
+# from email.MIMEText import MIMEText
+
+load_dotenv()
+with open('./accounts.json', 'rb') as f:
+    file_data = f.read()
+accounts = json.load(open('accounts.json'))
+
+msg = EmailMessage()
+msg['Subject'] = 'Follow Bot Has Finished'
+msg['From'] = os.getenv('GM_USERNAME')
+msg['To'] = os.getenv('GM_RECIPIENT')
+body = f'Follow bot has finished following {len(accounts)} accounts'
+msg.set_content(body)
+msg.add_attachment(file_data, maintype='application', subtype='octet-stream', filename='accounts.json')
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(os.getenv('GM_USERNAME'), os.getenv('GM_PASSWORD'))
+    smtp.send_message(msg)
+
+f.close()
+
+sys.stdout.flush()
