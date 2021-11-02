80dd707698ff4ee658bb751c6ce4e45c8e79bbd8
Griffin Davis <davisgriffinp@gmail.com>
2021-11-02 04:54:32 -0400

    ARM Implementation

--- index.js
+++ index.js
@@ -17,21 +17,22 @@ const options = {
 }
 
 async function startBot() {
+    // ARM Implementation
+    let browser = await puppeteer.launch({
+        headless: true,
+        executablePath: '/usr/bin/chromium-browser',
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    })
+    // let browser = await puppeteer.launch({ headless: false })
+    
     try {
-        // ARM Implementation
-        // let browser = await puppeteer.launch({
-        //     headless: true,
-        //     executablePath: '/usr/bin/chromium-browser',
-        //     args: ['--no-sandbox', '--disable-setuid-sandbox']
-        // })
-        let browser = await puppeteer.launch({ headless: false })
-
         const bot = await InstaBot.InstBot(options, browser)
 
         await bot.FollowBot()
         const pythonProcess = spawn('python', ['./sendEmail.py'])
         await browser.close()
     } catch (e) {
+        console.log('Error caught:')
         console.log(e)
         const [page] = await browser.pages()
 
